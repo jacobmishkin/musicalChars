7924cdb18816ee18effca92d4a4038ef3e0c549f
Rami Abraham <r@radesign.ws>
2016-09-30 10:35:25 -0400

    Add deps, add ajax github jsonp getter
    - Adds jQuery 3.1.1 and Tone.js dependencies
    - Defines jsonp getter for Github API v3 repo feeds
    - Misc inline docs for next step

--- assets/js/dev/app.js
+++ assets/js/dev/app.js
@@ -1,2 +1,61 @@
-// Nice, good work dude.
-'use strict'
+jQuery( document ).ready( function( $ ) {
+
+	'use strict'
+
+	/**
+	 * First you get the val
+	 */
+
+	var userOrOrg = $( 'input.user-or-org' ).val();
+	var repoName = $( 'input.repo-name' ).val();
+
+	$( 'button' ).on( 'click', function( event ) {
+		event.preventDefault();
+		console.table( userOrOrg, repoName )
+	} );
+
+	/**
+	 * The you get the feed
+	 */
+
+	/**
+	 * Github API v3 jsonp is returned in the format of:
+	 * https://api.github.com/repos/<user or organization>/<name of repository>/commits?callback=repo
+	 * (with the callback value being the jsonp payload)
+	 */
+	var userOrOrgExample = 'common-gold';
+	var repoNameExample = 'musical-chars';
+
+	$.ajax( {
+		url: 'https://api.github.com/repos/' + userOrOrgExample + '/' + repoNameExample + '/commits?callback=repo',
+		dataType: 'jsonp',
+		success: function( repo ) {
+			console.log( repo );
+			var text = '';
+			var len = repo.length;
+			for ( var i = 0; i < len; i++ ) {
+
+				text += '<p>' + title + '</p>'
+			}
+			$( '.output' ).html( text );
+
+
+			/**
+			 * Parse jsonp and calcuate various arbitrary musical properties, and assign them to the repoMusic object.
+			 *
+			 * @todo
+			 */
+
+			var repoMusic = {}
+
+
+			/**
+			 * Now we create music from the repoMusic values, using Tone.js
+			 *
+			 * @todo
+			 */
+
+		}
+	} )
+
+} );
